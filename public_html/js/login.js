042e3299a1992724a4137555686eb9cd44d75a1e
Daniel <dmg00024@gmail.com>
2017-04-17 12:43:48 +0200

    inicio de sesión con angularjs

---
+++ public_html/js/login.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+angular.module('authentication', [])
+        .controller('login', function ($scope, $http) {
+            
+            $scope.llamada = function () 
+            {
+                $http({
+                    method: 'GET',
+                    url: 'http://localhost:8080/RedSocialColaborativaRESTFUL/username/',
+                    
+                    xhrFields: {
+                        withCredentials: true
+                    },
+                    
+                    headers:{
+                        Authorization : "Basic " + btoa($scope.username + ":" + $scope.password)
+                    }
+                    
+                }).then(function succes(json)
+                {
+                    location.href='/RedSocialColaborativaClientAngularJS/miperfil.html';
+                    
+                }, function error(json){
+                    
+                    alert("Usuario y/o contraseña incorrectos");
+                });
+            };
+        });
